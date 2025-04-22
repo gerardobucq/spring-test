30997d2d46f281d869853fd4687d0f8081f0774e
gerardobucq <gbaranda36775@ucq.edu.mx>
2025-04-22 14:05:17 -0600

    Primer-Test

---
+++ src/main/java/com/example/holamundo/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.example.holamundo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String holaMundo() {
+        return "¡Testear!";
+    }
+}
